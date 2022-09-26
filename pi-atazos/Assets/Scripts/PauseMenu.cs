6c5d515a071b1233a5ff96a6d378002698d78a03
barbsromero1 <35925256+barbsromero1@users.noreply.github.com>
2022-09-26 02:32:34 -0500

    Pause Menu

--- pi-atazos/Assets/Scripts/PauseMenu.cs
+++ pi-atazos/Assets/Scripts/PauseMenu.cs
@@ -3,51 +3,104 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
+    public static bool activewristUI = false;
+    public GameObject wristUi;
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(GameIsPaused)
-            {
-                Resume();
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Escape))
+    //    {
+    //        if (GameIsPaused)
+    //        {
+    //            Time.timeScale = 1f;
+    //            GameIsPaused = false;
+    //            // Resume();
 
-            } else 
-            {
-                Pause();
-            }
+    //        }
+    //        else
+    //        {
+    //            Time.timeScale = 0f;
+    //            GameIsPaused = true;
+    //            // Pause();
+    //        }
+    //    }
+    //}
+
+    //public void Resume()
+    //{
+    //    pauseMenuUI.SetActive(false);
+    //    Time.timeScale = 1f;
+    //    GameIsPaused = false;
+    //}
+
+    //public void Pause()
+    //{
+    //    pauseMenuUI.SetActive(true);
+    //    Time.timeScale = 0f;
+    //    GameIsPaused = true;
+
+    //}
+
+    //public void LoadMenu()
+    //{
+    //    Time.timeScale = 1f;
+    //    SceneManager.LoadScene("Menu");
+    //}
+
+    //public void SettingsMenu()
+    //{
+    //    SceneManager.LoadScene("Settings");
+    //}
+
+    private void Start()
+    {
+        DisplayWristUI();
+    }
+
+    public void DisplayWristUI()
+    {
+        //pause
+        if (GameIsPaused)
+        {
+            wristUi.SetActive(true);
+            activewristUI = true;
+            Time.timeScale = 1;
+            GameIsPaused = true;
+            Debug.Log("Pause"); 
+        }
+        else if (!GameIsPaused)//not pause
+        {
+            wristUi.SetActive(false);
+            activewristUI = false;
+            Time.timeScale = 0;
+            GameIsPaused = false; 
         }
     }
 
-    public void Resume()
+    //Resume 
+    public void PauseButtonPressed(InputAction.CallbackContext context)
     {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
+        if (context.performed)
+        {
+            DisplayWristUI();
+        }
     }
 
-    void Pause()
+    public void RestarGame()
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    public void LoadMenu()
+    public void ExitGame()
     {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
+        Debug.Log("Exit");
+        //Application.Quit(); 
     }
 
-    public void SettingsMenu()
-    {
-        SceneManager.LoadScene("Settings");
-    }
 }
